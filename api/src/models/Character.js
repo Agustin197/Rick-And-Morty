a6b363e2dc13ca39a4b76fea9350122abb43240f
Agustín Ceballos <agustin.97.ceballos@gmail.com>
2022-11-24 23:16:34 -0300

    first commit

--- api/src/models/Character.js
+++ api/src/models/Character.js
@@ -4,6 +4,12 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("character", {
-
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
   });
 };
+
+
+//cambio
