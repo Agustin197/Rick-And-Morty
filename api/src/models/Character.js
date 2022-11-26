8407e91ff5c823cce8de7df170296c6af23787b1
Agustín Ceballos <agustin.97.ceballos@gmail.com>
2022-11-26 02:17:22 -0300

    model

--- api/src/models/Character.js
+++ api/src/models/Character.js
@@ -1,15 +1,36 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes, UUIDV4 } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("character", {
     id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+      primaryKey: true
+    },
+    name:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    species:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    origin:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    created:{
+      type: DataTypes.DATEONLY,
+      defaultValue: DataTypes.NOW,
+      allowNull: false
     },
   });
 };
 
 
-//cambios  2
